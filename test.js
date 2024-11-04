e743b3a4ef0c0b68cdef75056af6414ce9468c0c
Dexone <dm346@mail.ru>
2024-11-04 12:00:00 +0300

    upd

---
+++ test.js
@@ -0,0 +1,11 @@
+function solution(words) {
+
+    array.forEach(element => {
+        //object s kolichestvom bukv dlya kazhdoy prervoy i posledney bukvi
+    });
+
+    return true
+  }
+
+
+  console.log(solution(["excavate", "endure", "desire", "screen", "theater", "excess", "night"]))
